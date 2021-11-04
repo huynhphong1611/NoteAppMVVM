7474a5cc53370baba5a81331b21a4fb47c1c704f
huynhphong1611 <huynhvanphong123456@gmail.com>
2021-11-04 12:44:11 +0700

    them xoa note

--- app/src/main/java/com/hp/android/noteapp/activitys/MainActivity.kt
+++ app/src/main/java/com/hp/android/noteapp/activitys/MainActivity.kt
@@ -3,15 +3,16 @@ package com.hp.android.noteapp.activitys
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.hp.android.noteapp.R
+import com.hp.android.noteapp.databinding.ActivityMainBinding
 import com.hp.android.noteapp.model.Note
 import com.hp.android.noteapp.viewmodel.NoteViewModel
 
 class MainActivity : AppCompatActivity() {
-    private val noteViewMode: NoteViewModel by lazy {
+    private val noteViewModel: NoteViewModel by lazy {
         ViewModelProvider(
             this, NoteViewModel.NoteViewModelFactory(this.application)
         ).get(NoteViewModel::class.java)
@@ -19,19 +20,29 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // su dung databinding
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         init()
+
+        binding.btnAddNote.setOnClickListener {
+            val note = Note("title test", "content")
+            noteViewModel.insertNote(note)
+        }
+
+        binding.btnDeleteNote.setOnClickListener {
+            for (note in noteViewModel.getAllNote().value!!) {
+                noteViewModel.deleteNote(note)
+            }
+        }
     }
 
     fun init() {
-        noteViewMode.getAllNote().observe(this, Observer {
+        noteViewModel.getAllNote().observe(this, Observer {
             // update UI, vi nhu set adapter cho recycler view
+            Log.d("HuynhPVd", "init: size note " + it.size)
             Log.d("HuynhPVd", "init: " + it)
         })
-    }
-
-    fun addNote(view: View) {
-        val note = Note("title test", "content")
-        noteViewMode.insertNote(note)
     }
 }
